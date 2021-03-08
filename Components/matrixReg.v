d6e6e8997e40c99753fee2960ee65408cb8fbadd
ajhanley <72047867+ajhanley@users.noreply.github.com>
2021-03-08 15:22:34 -0500

    Added some initial ideas for the matrix register

---
+++ Components/matrixReg.v
@@ -0,0 +1,9 @@
+module matrixReg(x, y, z);
+//Matrix Register Holds x, horizontal dimension, y, the vertical dimension, and z, the actual matrixReg
+	input[7:0] x,y;
+	reg[x,y][7:0] matrix;//x by y 8 bit integers.
+
+
+
+
+endmodule
