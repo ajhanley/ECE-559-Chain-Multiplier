4a25a8670848f5b3f16c8fba105fbf10843f5276
ajhanley <72047867+ajhanley@users.noreply.github.com>
2021-04-29 13:59:23 -0400

    Updated Iteration to not produce the cases where i=j

--- Components/iteration_mod.v
+++ Components/iteration_mod.v
@@ -1,3 +1,4 @@
+// Code your design here
 module iterate(matlen,clk,reset,iw,jw,ir,jr,kr,rw);
   input[7:0] matlen;
   input clk;
@@ -12,23 +13,26 @@ module iterate(matlen,clk,reset,iw,jw,ir,jr,kr,rw);
     count2=0;
     count3=count2;
     for(x=matlen; x>0; x=x-1)begin
-      for(y=0; y<x; y=y+1)begin
+      for(y=0; y<x-1; y=y+1)begin
         for(z=y; z<x; z=z+1)begin
-          #10;//change this delay as needed to make sure its not updating too fast
-          if(z==x-2)
+          //change this delay as needed to make sure its not updating too fast
+          #5;
+          if(z==x-1)
          	eq=1;   
           else
             eq=0;
+          #5;
         end
       end
     end
+    eq=0;
     
   end
   
   assign iw = y;
-  assign jw = (matlen-x+y);
+  assign jw = (matlen-x+y+1);
   assign ir = y;
-  assign jr = (matlen-x+y);
+  assign jr = (matlen-x+y+1);
   assign kr = z;
   assign rw = eq;
             
@@ -37,25 +41,5 @@ endmodule
 /*
 // Code your testbench here
 // or browse Examples
-module tb();
-  reg[7:0] matlen;
-  reg clk;
-  reg reset;
-  wire[7:0] iw,jw,ir,jr,kr;
-  wire rw;
-  
-  iterate2 count(matlen, clk,reset, iw, jw, ir, jr, kr, rw);
-  always #20 clk=~clk;
-  initial begin
-    $dumpfile("dump.vcd");
-	$dumpvars(1);
-    clk=0;
-    matlen=8'b00000100;
-    reset=1;
-    #10;
-    reset=0;
-    #500;
-    $finish;
-  end
-endmodule
+
 */
