26d05906845bbef8d7400ebcf0399044aa1b7a8e
achen173 <54816128+achen173@users.noreply.github.com>
2021-05-11 18:56:50 -0400

    update main

--- Overview/main.py
+++ Overview/main.py
@@ -20,7 +20,12 @@ class MatrixChainDecider:
             self.computation_storage[x][x] = 0
 
     def get_comp(self):
-        print(self.computation_storage)
+        print(self.input_matrix_length)
+        print("\n Input: ",self.input_matrix, "\n Length of Input: ",len(self.input_matrix), "\n")
+        for row in self.computation_storage:
+            frmt = "{:>14}" * len(row)
+
+            print(frmt.format(*row))
         return self.computation_storage[0][self.input_matrix_length-1]  # max comp
         # print(self.computation_storage[self.input_matrix_length - 1][:self.input_matrix_length])    # for reording
 
@@ -33,15 +38,17 @@ class MatrixChainDecider:
                     first = self.computation_storage[i][k]
                     sec = self.computation_storage[k+1][j]
                     last = self.input_matrix[i]*self.input_matrix[k+1]*self.input_matrix[j+1]
-                    print(x, i, j)
+                    # print(x, i, j)
                     if first+sec+last < cost:
                         self.computation_storage[j][i] = k+1
                         cost = first+sec+last
                 self.computation_storage[i][j] = cost
 
-myMatrix = MatrixChainDecider(4)
+myMatrix = MatrixChainDecider(31)
 matrix = [10, 25, 15, 60, 30]
+# matrix = [255]*32
+# matrix = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2]
 myMatrix.insert_matrix(matrix)
 myMatrix.clear_storage()
 myMatrix.decider()
-print(myMatrix.get_comp())
+myMatrix.get_comp()
